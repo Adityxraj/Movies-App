e60cc3ff8f7a963a0c44a46d3a626fcb799b6b7a
Raj Aditya <adityxraj@gmail.com>
2022-02-19 11:28:27 +0530

    Part 2 Movies

--- src/Components/Movies.js
+++ src/Components/Movies.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
-import { movies } from "./getMovies";
+// import { movies } from "./getMovies";
+import axios from 'axios';
 
 export default class Movies extends Component {
   constructor() {
@@ -7,13 +8,56 @@ export default class Movies extends Component {
     this.state = {
       hover: "",
       parr: [1],
+      currPage: 1,
+      movies: [],
     };
   }
+  async componentDidMount() {
+    //side effects
+    // console.log(data)
+    // console.log('mounting done')
+    const res = await axios.get(`https://api.themoviedb.org/3/movie/popular?api_key=5540e483a20e0b20354dabc2d66a31c9&language=en-US&page=${this.state.currPage}`);
+    let data = res.data
+    this.setState({
+      movies: [...data.results]
+    })
+  }
+  changeMovies = async() => {
+    const res = await axios.get(`https://api.themoviedb.org/3/movie/popular?api_key=5540e483a20e0b20354dabc2d66a31c9&language=en-US&page=${this.state.currPage}`);
+    let data = res.data
+    this.setState({
+      movies: [...data.results]
+    })
+  }
+  handleRight = () => {
+    let temparr = []
+    for(let i=1; i <= this.state.parr.length + 1; i++){
+      temparr.push(i);
+    }
+    this.setState({
+      parr:[...temparr],
+      currPage: this.state.currPage + 1
+    },this.changeMovies)
+  }
+  handleLeft = () => {
+    if(this.state.currPage != 1){
+      this.setState({
+        currPage:this.state.currPage - 1
+      }, this.changeMovies)
+    }
+  }
+  handleClick = (value) => {
+    if(value != this.state.currPage){
+      this.setState({
+        currPage: value
+      }, this.changeMovies)
+    }
+  }
   render() {
-    let movie = movies.results;
+    // let movie = movies.results;
     return (
       <>
-        {movie.length == 0 ? (
+        {this.state.movies.length == 0 ? (
           <div class="spinner-border text-primary" role="status">
             <span class="visually-hidden">Loading...</span>
           </div>
@@ -23,7 +67,7 @@ export default class Movies extends Component {
               <strong>Trending</strong>
             </h3>
             <div className="movies-list">
-              {movie.map((movieObj) => (
+              {this.state.movies.map((movieObj) => (
                 <div
                   className="card movies-card"
                   onMouseEnter={() =>
@@ -69,19 +113,19 @@ export default class Movies extends Component {
               <nav aria-label="Page navigation example">
                 <ul class="pagination">
                 <li class="page-item">
-                    <a class="page-link" href="#">
+                    <a class="page-link" onClick={this.handleLeft}>
                       Previous
                     </a>
                   </li>
                   {this.state.parr.map((value) => (
                     <li class="page-item">
-                      <a class="page-link" href="#">
+                      <a class="page-link" onClick={() => this.handleClick(value)}>
                         {value}
                       </a>
                     </li>
                   ))}
                   <li class="page-item">
-                    <a class="page-link" href="#">
+                    <a class="page-link" onClick={this.handleRight}>
                       Next
                     </a>
                   </li>
